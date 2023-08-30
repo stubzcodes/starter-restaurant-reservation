1f3821aeaf7af39e04a713eb9d7b20c34a833497
Yesenia <yesi.garcia0519@gmail.com>
2023-08-30 18:28:00 -0400

    added table_id to tables table

--- back-end/src/db/migrations/20230828081215_createTablesTable.js
+++ back-end/src/db/migrations/20230828081215_createTablesTable.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable("tables", (table) => {
+    table.increments("table_id").primary();
     table.string("table_name").notNull();
     table.integer("capacity").notNull().unsigned();
     table.integer("reservation_id").defaultTo(null);
