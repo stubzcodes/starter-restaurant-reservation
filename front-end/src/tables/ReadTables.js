040d87f648568b38d0bac97e0ff056f3b7953ffb
Yesenia <yesi.garcia0519@gmail.com>
2023-09-08 19:27:29 -0400

    WIP add finish functionality

--- front-end/src/tables/ReadTables.js
+++ front-end/src/tables/ReadTables.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import ErrorAlert from "../layout/ErrorAlert";
 require("dotenv").config();
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 function ReadTables({ table }) {
+  const history = useHistory();
+  const [error, setError] = useState(null);
+
+  async function handleFinish(event) {
+    event.preventDefault();
+    const abortController = new AbortController();
+
+    if (
+      window.confirm(
+        "Is this table ready to seat new guests? This cannot be undone."
+      )
+    ) {
+      try {
+        await axios.put(
+          `${BASE_URL}/reservations/${table.reservation_id}/status`,
+          { data: { status: "finished" } },
+          abortController.signal
+        );
+        await axios.delete(
+          `${BASE_URL}/tables/${table.table_id}/seat`,
+          abortController.signal
+        );
+        history.push("/");
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          setError(error);
+        }
+      }
+      return () => abortController.abort();
+    }
+  }
 
   if (!table) {
     return <p>Loading...</p>;
   } else {
     return (
       <>
+        <ErrorAlert error={error} />
         <div className="card mb-3">
           <div className="card-body">
             <h5 className="card-title">Table: {table.table_name}</h5>
@@ -18,6 +54,17 @@ function ReadTables({ table }) {
               {" "}
               Status: {table.reservation_id === null ? "Free" : "Occupied"}
             </h6>
+            {table.reservation_id !== null ? (
+              <button
+                className="btn btn-primary mr-2"
+                data-table-id-finish={table.table_id}
+                onClick={handleFinish}
+              >
+                Finish
+              </button>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </>
